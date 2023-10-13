8e0372520b22196bab08a4d2c27cb3543ebd5bc4
Rahib.Web.Dev <rahib.web.dev@gmail.com>
2023-10-13 15:43:45 +0400

    First

--- 01.py
+++ 01.py
@@ -1,4 +1,21 @@
-nums = [20, 30, 15, 45, 12, 35, 28, 42, 79, 50]
+nums = [20, 30, 79, 45, 12, 35, 28, 42, 15, 50]
+
+# 1. Cəmi ən böyük olan iki massiv elementinin indexini göstərən funksiya yazın
+
+
+def find_biggest_sum_couple():
+    biggest_sum = 0
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] > biggest_sum:
+                biggest_sum = nums[i] + nums[j]
+                biggest_sum_couple = (i, j)
+    return biggest_sum_couple
+
+
+big_res = find_biggest_sum_couple()
+print("Cəmi ən böyük olan iki massiv elementinin indexi: ", big_res)
+
 
 # 2. Ən böyük ədədi tapan funksiya yazın
 
@@ -12,7 +29,7 @@ def max_num():
 
 
 result = max_num()
-print(f"En boyuk eded: {result}")
+print(f"Ən böyük ədəd: {result}")
 
 # 3. Massivdəki ən böyük ədədlə ən kiçik ədəd arasındakı fərqi tapın
 
@@ -36,7 +53,7 @@ def find_max_value():
 min_valu = find_min_value()
 max_valu = find_max_value()
 ferq = max_valu - min_valu
-print("Ferq:", ferq)
+print("Massivdəki ən böyük ədədlə ən kiçik ədəd arasındakı fərq: ", ferq)
 
 
 # def find_min_value(nums):
@@ -74,8 +91,8 @@ def tek_or_cut():
 
 
 tekler_cutler = tek_or_cut()
-print("Tekler:", tekler_cutler[0])
-print("Cutlar:", tekler_cutler[1])
+print("tək ədədlər: ", tekler_cutler[0])
+print("cüt ədədlər:", tekler_cutler[1])
 
 # 5. Massiv daxilində 20-dən böyük olan ədədləri silib yerdə qalan elementləri göstərən funksiya yazın
 
@@ -97,4 +114,4 @@ def elem_sil():
 
 
 res = elem_sil()
-print("20den boyukler: ", res)
+print("Massiv daxilində 20-dən böyük olan ədədlər: ", res)
